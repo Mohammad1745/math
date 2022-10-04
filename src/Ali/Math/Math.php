e82b50f0a48d2f10a869e52a8eb4547395a619d7
Mohammad1745 <mdali2016.227@gmail.com>
2022-10-04 15:28:07 +0600

    first commit

---
+++ src/Ali/Math/Math.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ali\Math;
+
+class Math
+{
+    public static function add($args)
+    {
+        $sum = 0;
+        foreach ($args as $arg) $sum += $arg;
+        return $sum;
+    }
+}
